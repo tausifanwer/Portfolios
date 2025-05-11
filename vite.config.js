4dbcdd94b2495a84108ba1785ed148e6602fd7e4
tausifanwer <tausifanwer89@gmail.com>
2025-05-11 16:57:31 +0530

    adding pwa

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,49 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-
+import { VitePWA } from "vite-plugin-pwa";
 // https://vite.dev/config/
 
 export default defineConfig({
-	plugins: [react(), tailwindcss()],
+	plugins: [
+		react(),
+		tailwindcss(),
+		VitePWA({
+			registerType: "autoUpdate",
+			devOptions: {
+				enabled: true,
+			},
+			manifest: {
+				name: "Tausif Portfolio",
+				short_name: "Portfolio",
+				start_url: "/",
+				display: "standalone",
+				background_color: "#ffffff",
+				theme_color: "#000000",
+				description: "Tausif Anwer's Portfolio",
+				icons: [
+					{
+						src: "/android-chrome-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "/android-chrome-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+					},
+					{
+						src: "/favicon-32x32.png",
+						sizes: "32x32",
+						type: "image/png",
+					},
+					{
+						src: "/favicon-16x16.png",
+						sizes: "16x16",
+						type: "image/png",
+					},
+				],
+			},
+		}),
+	],
 });
