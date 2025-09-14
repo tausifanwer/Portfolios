1d0a35fc5d187083cd4ef0222a65bafa66eecf80
Tausif Anwer <76683092+tausifanwer@users.noreply.github.com>
2025-09-14 15:20:36 +0530

    add:clearbot in about section (#2)

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.00l7c5m052o"
+    "revision": "0.nfbgq3idp6"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
