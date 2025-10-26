4e80f53ce090f514d6d010514936e58bd5774168
tausifanwer <tausifanwer89@gmail.com>
2025-10-26 13:03:23 +0530

    resume update pdf

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kh9alvo5b3g"
+    "revision": "0.7vh7bccbq2g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
