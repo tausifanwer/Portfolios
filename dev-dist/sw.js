d66a0531c5530b7def8fa19d89e4e45c90efed81
Tausif Anwer <76683092+tausifanwer@users.noreply.github.com>
2025-12-29 22:13:49 +0530

    UI fix (#9)
    * add-snowfall-on-top-of-all-components
    
    * added-dark-theme
    
    * modify-UI
    
    * fix-UI-part

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7dtq982vda"
+    "revision": "0.0419inndrgg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
